6a08c71f5b335f33884ece9c27cbbee5e425e059
TiagoDantas17 <tmgd2006@gmail.com>
2026-03-03 23:09:29 +0000

    Steimo_commit

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,23 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem; // Adicionado
 
 public class PlayerController : MonoBehaviour
 {
+
+    public InputAction moveAction;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        moveAction.Enable();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector2 position = transform.position; // Guarda a posição
 
-        position.x = position.x + 0.01f; // Move devagar para a direita
-        position.y = position.y + 0.01f; // Move devagar para cima
+        // Esta linha substitui todos os "ifs" anteriores!
+        Vector2 move = moveAction.ReadValue<Vector2>();
 
-        transform.position = position; // Aplica a nova posição
+        Vector2 position = transform.position;
+
+        // Movimento suave usando a nova MoveAction
+        position.x = position.x + 3.0f * move.x * Time.deltaTime;
+        position.y = position.y + 3.0f * move.y * Time.deltaTime;
+
+        transform.position = position;
     }
 }
