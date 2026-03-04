ebfa5259d127e034925e1fbd398245d166cbe69d
TiagoDantas17 <tmgd2006@gmail.com>
2026-03-04 11:59:32 +0000

    16_Commit

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,33 +5,37 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    // Variáveis relacionadas com o movimento do personagem
+    
     public InputAction moveAction;
     Rigidbody2D rigidbody2d;
     Vector2 move;
-
-    // Start é chamado antes do primeiro frame
+    public float speed = 3.0f;
+    public int maxHealth = 5;
+    int currentHealth;
     void Start()
     {
         moveAction.Enable();
-        // Obtém a referência do componente Rigidbody2D anexado à Ruby
+      
         rigidbody2d = GetComponent<Rigidbody2D>();
+        currentHealth = maxHealth;
     }
 
-    // Update é chamado uma vez por frame
+   
     void Update()
     {
-        // Lê o valor do input (teclas) e guarda na variável global 'move'
+
         move = moveAction.ReadValue<Vector2>();
     }
 
-    // FixedUpdate é usado para cálculos de física de forma estável
+    
     void FixedUpdate()
     {
-        // Calcula a nova posição baseada no Rigidbody para evitar o tremor (jittering)
-        Vector2 position = (Vector2)rigidbody2d.position + move * 3.0f * Time.deltaTime;
-
-        // Move o Rigidbody para a nova posição, respeitando as colisões
+        Vector2 position = (Vector2)rigidbody2d.position + move * speed * Time.deltaTime;
         rigidbody2d.MovePosition(position);
+    }
+    public void ChangeHealth(int amount)
+    {
+        currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
+        Debug.Log(currentHealth + "/" + maxHealth);
     }
 }
