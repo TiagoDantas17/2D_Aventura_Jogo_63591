712c16d651bb3a2d201a150ac284f0cf7c9cb6c4
TiagoDantas17 <tmgd2006@gmail.com>
2026-03-04 00:44:46 +0000

    14_Commit

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,29 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.InputSystem; // Adicionado
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-
+    // Variáveis relacionadas com o movimento do personagem
     public InputAction moveAction;
+    Rigidbody2D rigidbody2d;
+    Vector2 move;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    // Start é chamado antes do primeiro frame
     void Start()
     {
         moveAction.Enable();
+        // Obtém a referência do componente Rigidbody2D anexado à Ruby
+        rigidbody2d = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
+    // Update é chamado uma vez por frame
     void Update()
     {
-        // Lê o valor do input (teclas)
-        Vector2 move = moveAction.ReadValue<Vector2>();
+        // Lê o valor do input (teclas) e guarda na variável global 'move'
+        move = moveAction.ReadValue<Vector2>();
+    }
 
-        // Calcula a posição usando 'transform' (em inglês) e Time.deltaTime para velocidade constante
-        Vector2 position = (Vector2)transform.position + move * 3.0f * Time.deltaTime;
+    // FixedUpdate é usado para cálculos de física de forma estável
+    void FixedUpdate()
+    {
+        // Calcula a nova posição baseada no Rigidbody para evitar o tremor (jittering)
+        Vector2 position = (Vector2)rigidbody2d.position + move * 3.0f * Time.deltaTime;
 
-        // Aplica a nova posição ao objeto
-        transform.position = position;
+        // Move o Rigidbody para a nova posição, respeitando as colisões
+        rigidbody2d.MovePosition(position);
     }
 }
